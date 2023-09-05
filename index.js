9ea1b8224e1fbfbb27a8dccbf0df4f979d20c9a6
Swati <swatic019@gmail.com>
2023-09-05 18:13:32 +0530

    section css updated

---
+++ index.js
@@ -0,0 +1,57 @@
+  $("#carousel1").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots:true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+
+  $("#carousel2").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots:true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  
+  $("#carousel3").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots:true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+
+
